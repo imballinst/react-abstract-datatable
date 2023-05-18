b904026df64bbbf60655ec02ce4f63bd93629c73
Try Ajitiono <ballinst@gmail.com>
2023-05-18 04:11:13 +0000

    chore: sync examples

---
+++ scripts/sync-examples.sh
@@ -0,0 +1,8 @@
+# Assuming this is ran from the root project.
+cd examples/vite-react-bootstrap
+yarn add react-abstract-datatable@latest --exact
+
+cd ../vite-reactstrap
+yarn add react-abstract-datatable@latest --exact
+
+cd ../..
