8f9f92b34d32fa5e1a5c86426d113348406a4750
Try Ajitiono <ballinst@gmail.com>
2023-05-18 01:09:15 +0000

    chore: add stuff

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: [
+    '../src/__stories__/*.stories.mdx',
+    '../src/__stories__/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+  typescript: {
+    reactDocgen: false
+  }
+};
